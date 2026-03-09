05bcd2f0cdfd48e5b5436d4737877a009bb156bf
Topurayhan554 <topurayhantipu@gmail.com>
2026-03-09 12:45:06 +0600

    added routes and finalize frontend structure

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/Home/HomePage";
+import DashboardPage from "../pages/DashboardPage";
+import NotFoundPage from "../pages/NotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      { path: "dashboard", element: <DashboardPage /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
   },
 ]);
